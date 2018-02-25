df20be9bba2fc2c1406793df52738a853757d89c
sgcplay <frolkin.chepurnov@gmail.com>
2018-02-25 23:42:02 +0300

    2nd

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for (var i = 0; i < preferences.length; i++) {
+      var corner_1 = preferences[i] - 1;
+      var corner_2 = preferences[corner_1] - 1;
+      var corner_3 = preferences[corner_2] - 1;
+
+      if ((corner_3 == i) && (corner_1 != corner_2)) {
+
+          count++;
+          preferences[corner_1] = corner_1 + 1;
+
+          preferences[corner_2] = corner_2 + 1;
+
+          preferences[corner_3] = corner_3 + 1;
+      }
+  }
+  return count;
+
 };
